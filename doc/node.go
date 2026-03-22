2aaea1f68be6eb5a99e45ffb0eaedbd87eaa4398
Koppa Péter <koppa96@windowslive.com>
2026-03-22 14:13:33 +0100

    Add more docs

--- doc/node.go
+++ doc/node.go
@@ -4,7 +4,8 @@ import "io"
 
 // Node is an element of a Doc.
 type Node interface {
-	// FlatLength returns the length of the node if it is rendered Flat
+	// FlatLength returns the length of the node if it is rendered in a single line.
+	// If the node cannot be rendered in a single line, FlatLengt returns false in the second return variable.
 	FlatLength() (int, bool)
 
 	// Render renders the contents of the Node based on the context's state into the writer.
