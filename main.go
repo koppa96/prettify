f0f96d1a64d5252a1180348f5f153b5bfdf9c8b3
Koppa Péter <koppa96@windowslive.com>
2026-04-06 16:11:08 +0200

    Implement the parsing of assignment statements

--- main.go
+++ main.go
@@ -24,7 +24,9 @@ func Greet(name string) string {
 
 func MyMessageHandler(dep1 *dependencies.Dep1) func(ctx context.Context, msg *core.Message) error {
 	return func(ctx context.Context) error {
-		var cb func(ctx context.Context, dep1 *dependencies.Dep1, name string, age int) error
+		structVar := Person{
+			Name: "Peter",
+		}
 		return nil
 	}
 }`, parser.ParseComments)
